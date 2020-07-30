52f2f828ebbdf011a72259dbcf3a1c3c4a801ec2
shemar <dshemar6@gmail.com>
2020-07-29 20:18:58 -0400

    user data

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ import twint
 import csv
 from flask import Flask, jsonify, request
 
+
+def convertData(x):
+    return {'avi': x.avatar, 'followers:': x.followers, 'username': x.username}
 def convertTweet(x):
-    return {'link':x.link, 'tweet': x.tweet, 'username': x.username}
+    return {'link': x.link, 'tweet': x.tweet, 'username': x.username}
 
 
 f = open('badterms.csv')
@@ -15,6 +18,22 @@ app = Flask(__name__)
 
 for row in csv_f:
     tmpArray = row
+
+
+@app.route('/u/<username>')
+def show_user_avatar(username):
+    c = twint.Config()
+    c.Username = username
+    c.User_full = True
+    c.Store_object = True
+
+    twint.run.Lookup(c)
+    users = twint.output.users_list
+    
+    tweetsMap = list(map(convertData, users))
+    data.clear()
+    return jsonify(tweetsMap)
+
 
 @app.route('/t/<username>')
 def show_user_profile(username):
@@ -31,8 +50,3 @@ def show_user_profile(username):
     tweetsMap = list(map(convertTweet, data))
     data.clear()
     return jsonify(tweetsMap)
-
-   
-
-
-
